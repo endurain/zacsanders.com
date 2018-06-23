891d1cdc6778af507c9409ebcf0e0df2bedf5c19
endurain <zacharyjsanders@gmail.com>
2018-06-22 19:19:18 -0600

    Adding broken form information

--- src/components/Main.js
+++ src/components/Main.js
@@ -46,7 +46,7 @@ class Main extends React.Component {
 
         <article id="contact" className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">Contact</h2>
-          <form method="post" action="/main.js" netlify netlify-honeypot="bot-field">
+          <form method="post" action="#" netlify netlify-honeypot="bot-field">
             <div className="field half first">
               <label htmlFor="name">Name</label>
               <input type="text" name="name" id="name" />
