7a1bbeebb813088cf5a23897bdfd5158bf3d7fcf
zac <zacharyjsanders@gmail.com>
2018-07-07 11:44:25 -0600

    adding services info

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,23 +3,23 @@ import React from 'react'
 const Header = (props) => (
     <header id="header" style={props.timeout ? {display: 'none'} : {}}>
         <div className="logo">
-             
+
         </div>
         <nav>
             <ul>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Blog</a></li>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>Services</a></li>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
             </ul>
         </nav>
         <div className="content">
             <div className="inner">
-                <h1>ZS</h1>
+                <h1>ZS Web Solutions</h1>
                 <p>A Denver web development company</p>
             </div>
         </div>
-        
+
     </header>
 )
 
